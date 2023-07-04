2a14d46e978324680d97e41dcdbdea7fbec54b14
Paul Gessinger <hello@paulgessinger.com>
2023-07-04 20:55:55 +0200

    another fix

--- src/spyral/cli.py
+++ src/spyral/cli.py
@@ -118,7 +118,8 @@ def run(
                 for line in iter(p.stdout.readline, b""):
                     if not t.is_alive():
                         print("Monitoring thread has died")
-                        raise mon.exception
+                        print("Exception:", str(mon.exception))
+                        raise RuntimeError("Monitoring thread has died")
                     live.console.out(line.decode("utf-8"), highlight=False, end="")
         except KeyboardInterrupt:
             mon.terminate = True
